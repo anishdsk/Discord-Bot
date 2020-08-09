1301ab11ac2705a4031bf6252a2397c364754aa8
Anish Senthilkumar <anishdsk@yahoo.com>
2020-08-09 11:39:08 -0400

    added guild/server search and event handler for member join coroutine

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 
 # load env variables
 load_dotenv()
-# get discord token to allow access to API
-TOKEN = os.getenv('DISCORD_TOKEN')
+# get discord tokens to allow access to API and guild (server)
+TOKEN = os.getenv('DISCORD_TOKEN') # can only be used by who created the discord application
+GUILD = os.getenv('DISCORD_GUILD')
 
 # create discord client
 client = discord.Client()
@@ -13,6 +14,25 @@ client = discord.Client()
 # event handler for when connection is established
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord.')
+    # in case the bot is connected to multiple guilds, this will search for the guild we want
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(f'{client.user} is connected to guild: {guild.name} (id: {guild.id})')
 
+# event handler for new member join coroutine
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'{member.name}, welcome to the Discord server!'
+    )
+
+
+
+'''
+    # display list of users currently in the guild/server
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+'''
 client.run(TOKEN)
