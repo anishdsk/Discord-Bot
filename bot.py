59020c6843a52b97094251ec42cc65c349e7e04c
Anish Senthilkumar <anishdsk@yahoo.com>
2020-08-10 16:30:06 -0400

    Now uses Bot class. Added chatting, dice roll sim, and channel creation

--- bot.py
+++ bot.py
@@ -2,55 +2,75 @@ import os
 import discord
 import random
 from dotenv import load_dotenv
+from discord.ext import commands
 
-# load env variables
+# loads env variables
 load_dotenv()
-# get discord tokens to allow access to API and guild (server)
-TOKEN = os.getenv('DISCORD_TOKEN') # can only be used by who created the discord application
-
-# create discord client
-client = discord.Client()
+# get discord token to allow access to API
+'''
+Token subject to change. If you want to use this repo as a template to create
+your Discord Bot, after creating a Discord Application/Bot on the Discord
+Developer Portal, take the Token given when the Bot was created and replace
+the old token with your token in the '.env' file.
+'''
+TOKEN = os.getenv('DISCORD_TOKEN')
+# Creates Discord Bot. To invoke this bot, text commands towards it must be prefixed with '!'
+bot = commands.Bot(command_prefix='!')
 
 # event handler for when connection is established
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
 # event handler for new member join coroutine
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
         f'{member.name}, welcome to my Discord server!'
     )
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
+# event handler for when interaction with the bot occurs
+@bot.command(name='Hi', help='Responds with a greeting message')
+async def message_hi(ctx):
     # lists of responses to choose from
     greetings_responses = ['Hi', 'Hello', 'Greetings']
 
-    if message.content == 'Hello My_Bot' or message.content == 'Hi My_Bot':
-        response = random.choice(greetings_responses)
-        await message.channel.send(response)
-    # raises an exception when "raise-exception" is typed by the user; for testing purposes
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
+    response = random.choice(greetings_responses)
+    await ctx.send(response)
 
-# event handler for when errors are raised with on_message()
-# writes errors to external file for logging purposes
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+@bot.command(name='Hello', help='Responds with a greeting message')
+async def message_hi(ctx):
+    # lists of responses to choose from
+    greetings_responses = ['Hi', 'Hello', 'Greetings']
 
-'''
-    # display list of users currently in the guild/server
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
-'''
-client.run(TOKEN)
+    response = random.choice(greetings_responses)
+    await ctx.send(response)
+
+# simulates a dice roll
+@bot.command('roll_dice', help='Simulation of dice roll; type in # of dice & # of sides per die after command. Ex: !dice_roll 2 3')
+async def roll(ctx, numOfDice: int, numOfSides: int):
+    dice = [
+        str(random.choice(range(1, numOfSides + 1)))
+        for _ in range(numOfDice)
+    ]
+    await ctx.send(', '.join(dice))
+
+# only gives those with the server Admin ('admin') role to create new channels
+@bot.command(name='create-channel', help='Type !create-channel "channel name"')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name):
+    guild = ctx.guild
+    # makes sure the new channel does not have the same name as an existing one
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+# throws error when a user without the 'admin' role tries to make a new channel
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the role to execute this command.')
+
+bot.run(TOKEN)
