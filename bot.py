e05c2c96150416431076771e950aeb64356d1ee1
Anish Senthilkumar <anishdsk@yahoo.com>
2020-08-09 20:51:47 -0400

    Added basic chat features and error logging for on_message()

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import os
 import discord
+import random
 from dotenv import load_dotenv
 
 # load env variables
 load_dotenv()
 # get discord tokens to allow access to API and guild (server)
 TOKEN = os.getenv('DISCORD_TOKEN') # can only be used by who created the discord application
-GUILD = os.getenv('DISCORD_GUILD')
 
 # create discord client
 client = discord.Client()
@@ -14,21 +14,39 @@ client = discord.Client()
 # event handler for when connection is established
 @client.event
 async def on_ready():
-    # in case the bot is connected to multiple guilds, this will search for the guild we want
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-    print(f'{client.user} is connected to guild: {guild.name} (id: {guild.id})')
+    print(f'{client.user.name} has connected to Discord!')
 
 # event handler for new member join coroutine
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'{member.name}, welcome to the Discord server!'
+        f'{member.name}, welcome to my Discord server!'
     )
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    # lists of responses to choose from
+    greetings_responses = ['Hi', 'Hello', 'Greetings']
+
+    if message.content == 'Hello My_Bot' or message.content == 'Hi My_Bot':
+        response = random.choice(greetings_responses)
+        await message.channel.send(response)
+    # raises an exception when "raise-exception" is typed by the user; for testing purposes
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+# event handler for when errors are raised with on_message()
+# writes errors to external file for logging purposes
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
 
 '''
     # display list of users currently in the guild/server
